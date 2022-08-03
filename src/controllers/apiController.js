e3d42e67f452fa2d69738a82edac19a010dbf13b
kikerodrivela <kikerodrivela@gmail.com>
2022-08-03 15:52:08 +0200

    Add new endpoints

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -38,16 +38,30 @@ const postSensor = (req, res) => {
     // var id = req.body.id;
     var dispositivo = req.body.dispositivo;
 
+
     // Insert id and dispositivo into the database
     Sensores.create({
         dispositivo: dispositivo
-    }).then(() => {
-        res.sendStatus(201);
+    }).then(sensor => {
+        res.status(201).json(sensor);
+    }).catch(err => {
+        res.sendStatus(500);
+    });
+
+    // If dispositivo is equal to other dispositivo, return 409
+    Sensores.findOne({
+        where: {
+            dispositivo: dispositivo
+        }
+    }).then(sensor => {
+        if (sensor) {
+            res.sendStatus(409);
+        }
     }
     ).catch(err => {
         res.sendStatus(500);
     });
-
+    
     console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-" + "\n");
     console.log("Received time: " + timeString + "\n")
     console.log("Recieved a post request" + "\n");
@@ -65,10 +79,49 @@ const getSensores = (req, res) => {
     });
 }
 
+const getSensoresByID = (req, res) => {
+    Sensores.findOne({
+        where: {
+            id: req.params.id
+        }
+
+        // If id doesn't exist, return 404
+    }).then(sensor => {
+        if (!sensor) {
+            res.sendStatus(404);
+        } else {
+            res.status(200).json(sensor);
+        }
+    }
+    ).catch(err => {
+        res.sendStatus(500);
+    }
+    );
+}
+
+const deleteSensorById = (req, res) => {
+    Sensores.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(sensor => {
+        if (!sensor) {
+            res.sendStatus(404);
+        } else {
+            res.sendStatus(204);
+        }
+    }
+    ).catch(err => {
+        res.sendStatus(500);
+    });
+}
+
 
 module.exports = {
     apiWorks,
     postSensores,
     postSensor,
     getSensores,
+    getSensoresByID,
+    deleteSensorById
 }
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -6,5 +6,7 @@ router.get('/', ApiController.apiWorks);
 router.post('/sensores', ApiController.postSensores);
 router.post('/sensor', ApiController.postSensor);
 router.get('/sensores', ApiController.getSensores);
+router.get('/sensores/:id', ApiController.getSensoresByID);
+router.delete('/sensores/:id', ApiController.deleteSensorById);
 
 module.exports = router;
