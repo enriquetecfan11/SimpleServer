accf4683eabe46a141f0262e95ba85e523ad8500
kikerodrivela <kikerodrivela@gmail.com>
2023-02-02 09:05:34 +0100

    change thins in controller

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -30,31 +30,19 @@ const postMedidas = (req, res) => {
     if (winddirection >= 292.5 && winddirection < 337.5) return "NW"
   }
 
-  // console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-" + "\n");
-  // console.log("Received time: " + date.toLocaleTimeString() + "\n");
-  // console.log("Device: " + device + "\n");
-  // console.log("Device send time -> " + hora + "\n");
-  // console.log("Temperature: " + temp + "\n");
-  // console.log("Humidity: " + humidity + "\n");
-  // console.log("Rain: " + rain + "\n");
-  // console.log("Windspeed: " + windspeed + "\n");
-  // console.log("Winddirection: " + winddirection + "\n");
-  // console.log("Real Win Direction: " + discretizeWind(winddirection) + "\n");
-  // console.log("Luxes: " + luxes + "\n");
-  // console.log("Wifi RSSI: " + wifirrsi + "\n");
-  // console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-" + "\n");
-
-  // Only get 2 Temperature and Humidity Sensor
   console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-" + "\n");
   console.log("Received time: " + date.toLocaleTimeString() + "\n");
   console.log("Device: " + device + "\n");
-  console.log("Temperatura-1: " + temperatura);
-  console.log("Temperatura-2: " + temperatura1);
-  console.log("Humedad-1: " + humedad);
-  console.log("Humedad-2: " + humedad1);
-  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-" + "\n");
-
-
+  console.log("Device send time -> " + hora + "\n");
+  console.log("Temperature: " + temp + "\n");
+  console.log("Humidity: " + humidity + "\n");
+  console.log("Rain: " + rain + "\n");
+  console.log("Windspeed: " + windspeed + "\n");
+  console.log("Winddirection: " + winddirection + "\n");
+  console.log("Real Win Direction: " + discretizeWind(winddirection) + "\n");
+  console.log("Luxes: " + luxes + "\n");
+  console.log("Wifi RSSI: " + wifirrsi + "\n");
+  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-" + "\n");
 
   res.status(201).json(
     {
@@ -105,7 +93,12 @@ const postEstacion = (req, res) => {
   console.log("Wifi RSSI: " + wifirrsi + "\n");
   console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-" + "\n");
 
-  res.status(201).json(req.body); // Only for local debug
+  res.status(201).json(
+    {
+      "status": "OK",
+      "data": req.body
+    }
+  ); // Only for local debug
 }
 
 
@@ -132,33 +125,43 @@ const postTemperaturaSuelo = (req, res) => {
   console.log("Temperatura Subsuelo: " + temperaturaSubsuelo + "\n");
   console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-" + "\n");
 
-  res.status(201).json(req.body); // Only for local debug
+  res.status(201).json(
+    {
+      "status": "OK",
+      "data": req.body
+    }
+  ); // Only for local debug
 }
 
 // Dos sondas de temperatura y humedad ambiente
-const postTemperaturaDos = (req, res) =>{
+const postTemperaturaDos = (req, res) => {
   var date = new Date();
   var timeString = date.toLocaleTimeString();
   var device = req.body.dispositivo;
   var hora = req.body.hora;
   var temperatura = req.body.temperatura;
-  var humedad = req.body.humedad;
+  var humedad = req.body.humedadAire;
   var temperatura1 = req.body.temp1;
   var humedad1 = req.body.hum1;
-
   var data = JSON.stringify(req.body);
 
-  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-" + "\n");
-  console.log("Received time: " + date.toLocaleTimeString() + "\n");
-  console.log("Device: " + device + "\n");
-  console.log("Device send time -> " + hora + "\n");
-  console.log("Temperatura: " + temperatura + "\n");
-  console.log("Humedad: " + humedad + "\n");
-  console.log("Temperatura-1: " + temperatura1 + "\n");
-  console.log("Humedad-1: " + humedad1 + "\n");
-  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-" + "\n");
+  // {"dispositivo":"ESP32-1","hora":"2020-05-05T18:00:00","temperatura":25.5,"humedadAire":50.5,"temp1":30.1,"hum1":60}
+  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-");
+  console.log("Received time: " + date.toLocaleTimeString());
+  console.log("Device: " + device);
+  console.log("Device send time -> " + hora);
+  console.log("Temperatura: " + temperatura);
+  console.log("Humedad: " + humedad);
+  console.log("Temperatura 1: " + temperatura1);
+  console.log("Humedad 1: " + humedad1);
+  console.log("-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-");
 
-  res.status(201).json(req.body); // Only for local debug
+  res.status(201).json(
+    {
+      "status": "OK",
+      "data": req.body
+    }
+  ); // Only for local debug
 }
 
 module.exports = {
