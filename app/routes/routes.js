a4b1b03fd475abeb740229620dbcc414f0094212
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-12-09 15:08:36 +0100

    Refactor route for retrieving data from data.json

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -23,28 +23,25 @@ router.get('/status', (req, res) => {
 });
 
 router.get('/medidas', (req, res) => {
-  // Check if data.json exists and is ready to read
   fs.access('data.json', fs.F_OK)
     .then(() => {
-      // If it exists, read it
       fs.readFile('data.json')
         .then((data) => {
-          // Parse the data to JSON
           const dataJson = JSON.parse(data);
-          // Send the data as response
           res.status(200).json(dataJson);
+          console.log("data.json found");
+          console.log(dataJson);
         })
         .catch((err) => {
-          // If there was an error reading the file, send the error as response
           res.status(500).json(err);
         });
     })
     .catch(() => {
-      // If it doesn't exist, send a 404 error
       res.status(404).json({
         status: 404,
         message: 'data.json not found',
       });
+      console.log('data.json not found');
     });
 });
 
